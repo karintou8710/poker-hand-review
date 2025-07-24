3058b75d5df876ad50201924dbd658596f91687c
karintou8710 <karintou8710@gmail.com>
2025-07-25 00:43:47 +0900

    feat: PlaceHolder

--- src/ReviewEditor.tsx
+++ src/ReviewEditor.tsx
@@ -2,14 +2,17 @@ import Document from "@tiptap/extension-document";
 import Paragraph from "@tiptap/extension-paragraph";
 import Text from "@tiptap/extension-text";
 import { EditorProvider } from "@tiptap/react";
+import { Placeholder } from '@tiptap/extensions'
 import HandInput from "./extensitons/hand-input";
 import HardBreak from "@tiptap/extension-hard-break";
 
 import "./ReviewEditor.css";
 
-const extensions = [Document, Text, Paragraph, HandInput, HardBreak];
+const extensions = [Document, Text, Paragraph, HandInput, HardBreak, Placeholder.configure({
+      placeholder: 'ここにハンドレビューを入力してください。',
+    }),];
 
-const content = "<p>ここに入力してね</p>";
+const content = "";
 
 const ReviewEditor = () => {
   return (
